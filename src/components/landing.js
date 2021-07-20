d0014fd5be90bb65dc09e22ae40508ccd5ce187d
Miguel Sosa <msosadesign@gmail.com>
2021-07-20 08:01:11 +0200

    Make landing page fully responsive

--- src/components/landing.js
+++ src/components/landing.js
@@ -7,10 +7,11 @@ class LandingPage extends React.Component {
             <div className="landing">
                 <img src={settings} className="settings-icon" alt="settings icon" /><br></br>
                 <h1 className="h2">Focus Timer</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nisi, purus vulputate arcu semper nisl velit..</p>
+                <p className="body">A timer designed to keep you focused on what matters to you</p>
                 <div className="landing-fabs">
                     <button className="fab-secondary">Start Pause (15min)</button>
                     <button className="fab">Start Focus (1hr)</button>
+
                 </div>
             </div>
         )
